4a71cb9a7da2af5178971d7634708c2fb531bf4a
Engindeniz00 <engindenizmuhendis@gmail.com>
2022-10-28 01:08:07 +0300

    Example led control c# ui add

---
+++ LED_CONTROL/LED_CONTROL/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace LED_CONTROL
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnConnect.Location = new System.Drawing.Point(281, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(227, 87);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "CONNECT";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(144, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(504, 38);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(144, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 66);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "LED1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(405, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(243, 66);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "LED2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(144, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(243, 66);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "LED3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(405, 290);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(243, 66);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "LED4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ LED_CONTROL/LED_CONTROL/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LED_CONTROL
+{
+    public partial class Form1 : Form
+    {
+        SerialPort _serialPort = new SerialPort("COM7", 9600, Parity.None, 8, StopBits.One);
+        
+        public Form1()
+        {
+            InitializeComponent();
+            _serialPort.Handshake = Handshake.None;
+            _serialPort.WriteTimeout = 500;
+        }
+
+        
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // Makes sure serial port is open before trying to write  
+            try
+            {
+                if (!(_serialPort.IsOpen))
+                    _serialPort.Open();
+                //_serialPort.Write("SI\r\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening/writing to serial port :: " + ex.Message, "Error!");
+            }
+        }
+        private delegate void SetTextDeleg(string text);
+        void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Thread.Sleep(500);
+            string data = _serialPort.ReadLine();
+            // Invokes the delegate on the UI thread, and sends the data that was received to the invoked method.  
+            // ---- The "si_DataReceived" method will be executed on the UI thread which allows populating of the textbox.  
+            this.BeginInvoke(new SetTextDeleg(si_DataReceived), new object[] { data });
+        }
+        private void si_DataReceived(string data) { textBox1.Text = data.Trim(); }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.BackColor == SystemColors.Control) 
+            {
+                _serialPort.Write("led1 on");
+                button1.BackColor = Color.Coral;
+            }
+            else 
+            {
+                _serialPort.Write("led1 off");
+                button1.BackColor = SystemColors.Control;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button1.BackColor == SystemColors.Control)
+            {
+                _serialPort.Write("led2 on");
+                button1.BackColor = Color.Coral;
+            }
+            else
+            {
+                _serialPort.Write("led2 off");
+                button1.BackColor = SystemColors.Control;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button1.BackColor == SystemColors.Control)
+            {
+                _serialPort.Write("led3 on");
+                button1.BackColor = Color.Coral;
+            }
+            else
+            {
+                _serialPort.Write("led3 off");
+                button1.BackColor = SystemColors.Control;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button1.BackColor == SystemColors.Control)
+            {
+                _serialPort.Write("led4 on");
+                button1.BackColor = Color.Coral;
+            }
+            else
+            {
+                _serialPort.Write("led4 off");
+                button1.BackColor = SystemColors.Control;
+            }
+        }
+    }
+}
